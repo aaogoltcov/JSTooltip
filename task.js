aed5c4fcd137921061993a474c189ce3fb2a43e3
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-08-14 01:21:17 +0300

    mistakes resolved

--- task.js
+++ task.js
@@ -4,13 +4,17 @@ const toolTipList = Array.from(document.querySelectorAll('a.has-tooltip'));
 
 toolTipList.forEach(function (tip) {
     tip.onclick = function () {
-        try {
-            document.querySelector('div.tooltip_active').remove();
-        } catch (e) {
-        };
         let topCoordinate = tip.getBoundingClientRect().top < window.innerHeight / 2 ?  tip.getBoundingClientRect().bottom + 10 : tip.getBoundingClientRect().top - 30;
         let leftCoordinate = tip.getBoundingClientRect().left < window.innerWidth / 2 ? tip.getBoundingClientRect().left + 10 : tip.getBoundingClientRect().left - 10;
-        tip.insertAdjacentHTML('afterend', `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`);
+        let html = `<div class="tooltip tooltip_active" style="left: ${leftCoordinate}px; top: ${topCoordinate}px">${tip.title}</div>`;
+        let activeTooltip = document.querySelector('div.tooltip_active');
+        if (activeTooltip) {
+            activeTooltip.style.left = `${leftCoordinate}px`;
+            activeTooltip.style.top = `${topCoordinate}px`;
+            activeTooltip.textContent = `${tip.title}`;
+        } else {
+            tip.insertAdjacentHTML('afterend', html);
+        };
         return false;
     };
 });
